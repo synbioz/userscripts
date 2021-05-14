5dc757695e82e0fad6db22208197c21c00f382ed
Martin Catty <mcatty@synbioz.com>
2021-05-14 19:23:09 +0200

    feat: observe labels mutation and re-render proposal
    We now watch labels mutation and render proposal every time they change.
    
    That means we can now chain label's updates. Clicking on a proposal will
    update label which in turn will update proposal.

--- gitlab_label_proposal.js
+++ gitlab_label_proposal.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Gitlab label proposal
 // @namespace    https://www.synbioz.com/
-// @version      0.2.0
+// @version      0.3.0
 // @description  Label proposal for gitlab
 // @author       Martin Catty
 // @include      /^https:\/\/git\.synbioz\.com/.*/issues/\d
@@ -23,41 +23,100 @@
     ["to deploy::staging", "to deploy::production"]
   ];
 
-  const initialStates = states.map(node => node[0]);
-  const transitions = new Map(states);
-  const label = document.querySelector(".labels");
-
-  // Create a map from existing labels, using this form :
-  // { "label" => [node1, node2] }
-  const labelsToNodes = Array.from(document.querySelectorAll(".labels .gl-label-text")).reduce((map, node) => {
-    const sibling = node.nextElementSibling;
-    let scope = "";
-
-    if (sibling !== undefined && sibling !== null) {
-      scope = "::" + sibling.textContent.trim();
-    }
-
-    const content = node.textContent.trim() + scope;
-
-    // we don't want to keep track of label's node not part
-    // of the states we are watching
-    if (initialStates.includes(content)) {
-      map.set(content, [node, sibling].compact());
-    }
-
-    return map;
-  }, new Map());
-
-  const nextLabels = Array.from(labelsToNodes.keys()).map(label => transitions.get(label)).compact().flat();
-
   const header = `
       <div class="title hide-collapsed gl-mb-3">
         Labels proposal
       </div>
     `;
 
-  const html = nextLabels.reduce((acc, label) => {
-    acc += `
+  const initialStates = states.map(node => node[0]);
+  const transitions = new Map(states);
+  const label = document.querySelector(".labels");
+  const csrf = document.querySelector("meta[name=\"csrf-token\"]").getAttribute("content");
+
+  function renderLabelsProposal() {
+    const labelsToNodes = mapExistingLabelsToStates();
+    const nextLabels = Array.from(labelsToNodes.keys()).map(label => transitions.get(label)).compact().flat();
+
+    clearExistingLabelsProposal();
+
+    const html = buildNextLabelsHTML(nextLabels);
+
+    appendBlock(html);
+    registerLabelsProposalListener(labelsToNodes);
+  }
+
+  function clearExistingLabelsProposal() {
+    document.querySelectorAll(".labels-proposal").forEach(node => node.remove());
+  }
+
+  function registerLabelsProposalListener(labelsToNodes) {
+    document.querySelectorAll(".label-proposal").forEach(node => {
+      node.addEventListener("click", event => {
+        event.preventDefault();
+
+        const label = event.target.textContent.trim();
+        const url = `/api/v4/projects/${document.body.dataset.projectId}/issues/${document.body.dataset.pageTypeId}?add_labels=${encodeURIComponent(label)}`;
+
+        const headers = new Headers({
+          "Content-Type": "application/json",
+          "x-csrf-token": csrf,
+          Accept: "application/json"
+        });
+
+        fetch(url, {
+          method: "PUT",
+          headers: headers,
+          withCredentials: true
+        }).then(response => {
+          if (response.ok) {
+            node.remove();
+
+            // remove existing labels
+            labelsToNodes.forEach((nodes, key) => {
+              // there can be 2 nodes (including the scope), we only need one,
+              // they have the same parent
+              const node = nodes[0];
+              const cross = node.parentNode.nextElementSibling;
+
+              if (cross) {
+                const event = new Event("click");
+
+                cross.dispatchEvent(event);
+              }
+            });
+          }
+        });
+      });
+    });
+  }
+
+  // Create a map from existing labels, using this form :
+  // { "label" => [node1, node2] }
+  function mapExistingLabelsToStates() {
+    return Array.from(document.querySelectorAll(".labels .gl-label-text")).reduce((map, node) => {
+      const sibling = node.nextElementSibling;
+      let scope = "";
+
+      if (sibling !== undefined && sibling !== null) {
+        scope = "::" + sibling.textContent.trim();
+      }
+
+      const content = node.textContent.trim() + scope;
+
+      // we don't want to keep track of label's node not part
+      // of the states we are watching
+      if (initialStates.includes(content)) {
+        map.set(content, [node, sibling].compact());
+      }
+
+      return map;
+    }, new Map());
+  }
+
+  function buildNextLabelsHTML(nextLabels) {
+    return nextLabels.reduce((acc, label) => {
+      acc += `
           <div class="hide-collapsed value issuable-show-labels js-value has-labels">
             <span data-qa-selector="selected_label_content" data-qa-label-name="to do" class="gl-label gl-label-text-light"
               style="--label-background-color:#D10069; --label-inset-border:inset 0 0 0 2px #D10069;">
@@ -70,53 +129,28 @@
           </div>
         `;
 
-    return acc;
-  }, header);
+      return acc;
+    }, header);
+  }
 
-  const block = document.createElement("div");
+  function appendBlock(html) {
+    const block = document.createElement("div");
 
-  block.className = "block";
-  label.after(block);
-  block.innerHTML = html;
+    block.className = "block labels-proposal";
+    label.after(block);
+    block.innerHTML = html;
+  }
 
-  const csrf = document.querySelector("meta[name=\"csrf-token\"]").getAttribute("content");
-
-  document.querySelectorAll(".label-proposal").forEach(node => {
-    node.addEventListener("click", event => {
-      event.preventDefault();
-
-      const label = event.target.textContent.trim();
-      const url = `/api/v4/projects/${document.body.dataset.projectId}/issues/${document.body.dataset.pageTypeId}?add_labels=${encodeURIComponent(label)}`;
-
-      const headers = new Headers({
-        "Content-Type": "application/json",
-        "x-csrf-token": csrf,
-        Accept: "application/json"
-      });
-
-      fetch(url, {
-        method: "PUT",
-        headers: headers,
-        withCredentials: true
-      }).then(response => {
-        if (response.ok) {
-          node.remove();
-
-          // remove existing labels
-          labelsToNodes.forEach((nodes, key) => {
-            // there can be 2 nodes (including the scope), we only need one,
-            // they have the same parent
-            const node = nodes[0];
-            const cross = node.parentNode.nextElementSibling;
-
-            if (cross) {
-              const event = new Event("click");
-
-              cross.dispatchEvent(event);
-            }
-          });
-        }
-      });
-    });
+  // re-render proposal when labels change
+  const observer = new MutationObserver(mutations => {
+    for (const mutation of mutations) {
+      if (mutation.target.classList.contains("issuable-show-labels")) {
+        renderLabelsProposal();
+      }
+    }
   });
+
+  observer.observe(label, { attributes: false, childList: true, subtree: true });
+
+  renderLabelsProposal();
 })();
